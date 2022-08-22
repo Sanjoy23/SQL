b84b5dcf412af70f2dbf21580576e533a70eb6a0
mhhrakib <mhrakibdu@gmail.com>
2022-08-22 17:10:59 +0600

    add new query

--- Learning Sql SQLZOO.sql
+++ Learning Sql SQLZOO.sql
@@ -53,3 +53,11 @@ where continent = 'South America';
 select name, round(gdp / population, -3)
 from world
 where gdp > 1000000000000;
+
+---11. Greece has capital Athens.
+--- Each of the strings 'Greece', and 'Athens' has 6 characters.
+--- Show the name and capital where the name and the capital have the same number of characters.
+
+SELECT name,capital
+FROM world
+WHERE Len(capital) = len(name);
